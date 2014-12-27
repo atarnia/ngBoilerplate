82188ea729c97f5422c7fc64ddb7e81c8aedae03
Michal Karzyński <bitbucket@karzyn.com>
2014-12-27 12:03:40 +0100

    Gulp task - Serve

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp'),
-  del = require('del'),
-  wrap = require('gulp-wrap'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  jshint = require('gulp-jshint'),
-  sourcemaps = require('gulp-sourcemaps'),
-  gulpTemplate = require('gulp-template'),
-  fileSort = require('gulp-angular-filesort');
-  templateCache = require('gulp-angular-templatecache');
+    path = require('path'),
+    del = require('del'),
+    wrap = require('gulp-wrap'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    sourcemaps = require('gulp-sourcemaps'),
+    gulpTemplate = require('gulp-template'),
+    fileSort = require('gulp-angular-filesort'),
+    templateCache = require('gulp-angular-templatecache'),
+    webserver = require('gulp-webserver');
 
 var paths = {
   appScripts:    ['app/app.js', 'app/**/*.js'],
@@ -31,49 +33,65 @@ gulp.task('clean', function (cb) {
 // Compile Angular templates into a JS module
 gulp.task('templates', function (cb) {
   return gulp.src(paths.appTemplates)
-    .pipe(templateCache())
-	.pipe(wrap('angular.module("templates", []); <%= contents %>'))
-    .pipe(gulp.dest(paths.tmp));
+      .pipe(templateCache())
+      .pipe(wrap('angular.module("templates", []); <%= contents %>'))
+      .pipe(gulp.dest(paths.tmp));
 });
 
 // Concatenate and minify all app scripts and templates into app.js
 gulp.task('scripts-app', ['templates'],  function() {
   return gulp.src([].concat(paths.tmp+'templates.js', paths.appScripts))
-    .pipe(fileSort())
-	.pipe(jshint())
-	.pipe(jshint.reporter('jshint-stylish'))
-	.pipe(wrap('(function(){\n"use strict";\n<%= contents %>\n})();'))
-    .pipe(sourcemaps.init())
-    .pipe(uglify())
-    .pipe(concat('app.js'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.build));
+      .pipe(fileSort())
+      .pipe(jshint())
+      .pipe(jshint.reporter('jshint-stylish'))
+      .pipe(wrap('(function(){\n"use strict";\n<%= contents %>\n})();'))
+      .pipe(sourcemaps.init())
+      .pipe(uglify())
+      .pipe(concat('app.js'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(paths.build));
 });
 
 // Concatenate all vendor scripts
 gulp.task('scripts-vendor', function() {
   return gulp.src([].concat(paths.vendorScripts))
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest(paths.build));
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest(paths.build));
 });
 
 // Create index.html bootstrap file
 gulp.task('index', function() {
   return gulp.src(paths.indexHtml)
-    .pipe(gulpTemplate({
-      vendorCss: 'vendor.css',
-      appCss: 'app.css',
-      vendorJs: 'vendor.js',
-      appJs: 'app.js',
-    }))
-    .pipe(gulp.dest(paths.build))
+      .pipe(gulpTemplate({
+        vendorCss: 'vendor.css',
+        appCss: 'app.css',
+        vendorJs: 'vendor.js',
+        appJs: 'app.js',
+      }))
+      .pipe(gulp.dest(paths.build))
 });
 
 // Watch for file changes and execute appropriate tasks
 gulp.task('watch', function(){
-	gulp.watch([].concat(paths.appScripts, paths.appTemplates), ['scripts-app']);
-	gulp.watch([].concat(paths.vendorScripts), ['scripts-vendor']);
+  gulp.watch([].concat(paths.appScripts, paths.appTemplates), ['scripts-app']);
+  gulp.watch([].concat(paths.vendorScripts), ['scripts-vendor']);
 });
+
+
+gulp.task('serve', function() {
+  gulp.src(path.resolve(paths.build))
+      .pipe(webserver({
+        port: 4200,
+        livereload: true,
+        //directoryListing: true,
+        open: true,
+        proxies: [
+          {source: '/api', target: 'http://127.0.0.1:8000/api' },
+          {source: '/static', target: 'http://127.0.0.1:8000/static' }
+        ]
+      }));
+});
+
 
 // Build application
 gulp.task('build', ['scripts-vendor', 'scripts-app', 'index']);
